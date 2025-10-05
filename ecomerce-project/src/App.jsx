8a16ed9331f44c7b99a2bd987f8544c095eb798b
teumaywerashe <teumay21@gmail.com>
2025-10-05 19:18:58 +0300

    creating react-route

--- ecomerce-project/src/App.jsx
+++ ecomerce-project/src/App.jsx
@@ -1,16 +1,14 @@
-// import { useState } from "react";
+import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />}/>
+      <Route path="checkout" element={<div>test checkout page</div>}/>
+    </Routes>
   );
 }
 
